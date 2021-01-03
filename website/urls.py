78dedf2966f705127e1929804d2b7ad2dfae4bf0
George Testo <metro1102@gmail.com>
2021-01-02 22:54:40 -0700

    :mag: Add sitemap generation
    Adds basic sitemap generation functionality for future search engine optimization (SEO).

--- website/urls.py
+++ website/urls.py
@@ -14,17 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
+
+    path('sitemap.xml', sitemap),
+
     path('', include('core.urls')),
+
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('', include(wagtail_urls)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
